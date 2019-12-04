e4f8b5546e101cedc64d596f19a69329b5ac51bc
littlecat <5234847+littlecat0803@user.noreply.gitee.com>
2019-12-04 16:46:44 +0800

    调用百度AI的API并渲染页面

--- app.js
+++ app.js
@@ -1,39 +1,20 @@
 //app.js
 App({
-  onLaunch: function () {
-    // 展示本地存储能力
-    var logs = wx.getStorageSync('logs') || []
-    logs.unshift(Date.now())
-    wx.setStorageSync('logs', logs)
-
-    // 登录
-    wx.login({
-      success: res => {
-        // 发送 res.code 到后台换取 openId, sessionKey, unionId
-      }
-    })
-    // 获取用户信息
-    wx.getSetting({
-      success: res => {
-        if (res.authSetting['scope.userInfo']) {
-          // 已经授权，可以直接调用 getUserInfo 获取头像昵称，不会弹框
-          wx.getUserInfo({
-            success: res => {
-              // 可以将 res 发送给后台解码出 unionId
-              this.globalData.userInfo = res.userInfo
-
-              // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
-              // 所以此处加入 callback 以防止这种情况
-              if (this.userInfoReadyCallback) {
-                this.userInfoReadyCallback(res)
-              }
+    globalData: {
+        access_token:''
+    },
+    onLaunch: function() {
+        this.globalData.access_token='aaa'
+        wx.request({
+            method:'post',
+            url: 'https://aip.baidubce.com/oauth/2.0/token?grant_type=client_credentials&client_id=vnKtFP0LDOFkAiQzN6r8io9o&client_secret=Dv6rxOQh56uRSu3HSK3TAGRoSt9cSPHZ',
+            success:(res)=>{
+                this.globalData.access_token = res.data.access_token;
+                console.log(this.globalData.access_token)
+            },
+            fail:()=>{
+                console.log("失败了")
             }
-          })
-        }
-      }
-    })
-  },
-  globalData: {
-    userInfo: null
-  }
+        })
+    }
 })
--- pages/faceMark/faceMark.js
+++ pages/faceMark/faceMark.js
@@ -1,20 +1,146 @@
+const app = getApp()
+// console.log(app.globalDada);
 Page({
     data:{
-        windowHeight:0
+        windowHeight:0,
+        // 摄像头朝向
+        position:"back",
+        // 照片的路径
+        src: '',
+        // 是否展示选择的照片
+        isShowPic: false,
+        isShowBox: false,
+        // 人脸信息
+        faceInfo: null,
+        map: {
+            gender: {
+                male: '男',
+                female: '女'
+            },
+            expression: {
+                none: '平静', smile: '微笑', laugh: '大笑'
+            },
+            glasses: {
+                none: '无眼镜', common: '普通眼镜', sun: '墨镜'
+            },
+            emotion: {
+                angry: '愤怒', disgust: '厌恶', fear: '恐惧', happy: '高兴',
+                sad: '伤心', surprise: '惊讶', neutral: '无情绪'
+            }
+        }
     },
+    // 点击按钮，切换摄像头
+    reverseCamera() {
+        const newPosition = this.data.position === 'back' ? 'backfront' : 'back'
+        this.setData({
+            position: newPosition
+        })
+    },
+    // 拍照
     takePhoto() {
+        // 创建相机的实例对象
         const ctx = wx.createCameraContext()
+        // ctx.takePhoto 实现拍照
         ctx.takePhoto({
             quality: 'high',
             success: (res) => {
+                console.log(res.tempImagePath)
                 this.setData({
-                    src: res.tempImagePath
+                    src: res.tempImagePath,
+                    isShowPic: true
+                }, () => {
+                    this.getFaceInfo()
+                })
+            },
+            fail: () => {
+                console.log('拍照失败！')
+                this.setData({
+                    src: ''
+                })
+            }
+        })
+        console.log(this.data.src);
+    },
+    choosePhoto(){
+        wx.chooseImage({
+            count: 1,
+            sizeType: ['original'],
+            sourceType: ['album'],
+            success: (res) => {
+                if (res.tempFilePaths.length > 0) {
+                    this.setData({
+                        src: res.tempFilePaths[0],
+                        isShowPic: true
+                    }, () => {
+                        this.getFaceInfo()
+                    })
+                }
+                console.log(this.data.src)
+
+            },
+            fail: () => {
+                console.log('选择照片失败！')
+                this.setData({
+                    src: ''
                 })
             }
         })
     },
-    error(e) {
-        console.log(e.detail)
+    reChoose(){
+        this.setData({
+            isShowPic: false,
+            src: '',
+            isShowBox: false
+        })
+    },
+    getFaceInfo() {
+        const token = app.globalData.access_token
+        // console.log(token)
+        if (!token) {
+            return wx.showToast({
+                title: '鉴权失败！',
+            })
+        }
+        wx.showLoading({
+            title: '颜值检测中...',
+        })
+        const fileManager = wx.getFileSystemManager()
+        const fileStr = fileManager.readFileSync(this.data.src, 'base64')
+        // console.log(fileStr);
+        wx.request({
+            method: 'POST',
+            url: 'https://aip.baidubce.com/rest/2.0/face/v3/detect?access_token=' + token,
+            header: {
+                'Content-Type': 'application/json'
+            },
+            data: {
+                image_type: 'BASE64',
+                image: fileStr,
+                // 年龄,颜值分数,表情,性别,是否戴眼镜,情绪
+                face_field: 'age,beauty,expression,gender,glasses,emotion'
+            },
+            success: (res) => {
+                console.log(res)
+                if (res.data.result.face_num <= 0) {
+                    return wx.showToast({
+                        title: '未检测到人脸！',
+                    })
+                }
+
+                this.setData({
+                    faceInfo: res.data.result.face_list[0],
+                    isShowBox: true
+                })
+            },
+            fail: () => {
+                wx.showToast({
+                    title: '颜值检测失败！',
+                })
+            },
+            complete: () => {
+                wx.hideLoading()
+            }
+        })
     },
     onLoad:function(options){
         const systemInfo = wx.getSystemInfoSync();
